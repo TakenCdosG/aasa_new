cd42ccf1a733045da452584303e15985870854ed
Adrián Andres Morelos Henriquez <ing.adrianmorelos@gmail.com>
2016-03-18 10:52:48 -0500

    Ajustes

--- sites/all/modules/custom/aasa_renewals/templates/manage-renewal-process.tpl.php
+++ sites/all/modules/custom/aasa_renewals/templates/manage-renewal-process.tpl.php
@@ -1,3 +1,7 @@
+<?php if(isset($info["environment"])): ?>
+	<?php echo $info["environment"]; ?>
+<?php endif; ?>
+
 <?php if(isset($info["renewal_period"])): ?>
 	<h4> Renewal Period.</h4>
 	<?php echo $info["renewal_period"]; ?>
