2f1a15ddd124778487d3d8fbb235cd986a6ad81d
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2016-08-25 15:11:40 -0500

    Ajustes

--- sites/all/modules/custom/aasa_content/templates/board-members-bios.tpl.php
+++ sites/all/modules/custom/aasa_content/templates/board-members-bios.tpl.php
@@ -5,18 +5,18 @@
 ?>
 
 <?php foreach ($bios as $key => $bio): ?>
+<label for="edit-submitted-vice-president"><?php echo $bio['category_label']; ?></label>
 <div class="accordion-members board-members-bios-container">
-	<label for="edit-submitted-vice-president"><?php echo $bio['category_label']; ?></label>
 	<?php $members = $bio['category_bios']; ?>
 	<?php foreach ($members as $k => $member): ?>
-		<h4 class="panel-title">
-         <a href="#accordion<?php echo $bio['category_tid']; ?>" data-parent="#accordion<?php echo $bio['category_tid']; ?>" data-toggle="collapse" class="accordion-toggle" aria-expanded="true">
-           <?php echo $member->title; ?>
-         </a>
-      </h4>
-      <div class="panel-body">
+		<h3 class="panel-title">
+	         <a href="#accordion<?php echo $bio['category_tid']; ?>" data-parent="#accordion<?php echo $bio['category_tid']; ?>" data-toggle="collapse" class="accordion-toggle" aria-expanded="true">
+	           <?php echo $member->title; ?>
+	         </a>
+      	</h3>
+      	<div class="panel-body">
          <?php echo $member->body['und'][0]['value']; ?>
-      </div>
+     	 </div>
 	<?php endforeach; ?>
 </div>
 <?php endforeach;?>
