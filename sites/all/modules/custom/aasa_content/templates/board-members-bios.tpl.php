c3f34f1d447d1811d7ae193e3072ba9957d4f2db
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2016-08-25 15:58:43 -0500

    Ajustes

--- sites/all/modules/custom/aasa_content/templates/board-members-bios.tpl.php
+++ sites/all/modules/custom/aasa_content/templates/board-members-bios.tpl.php
@@ -10,15 +10,17 @@
 		<?php if(count($members)>0): ?>
 		<label for="edit-submitted-vice-president"><?php echo $bio['category_label']; ?></label>
 		<div class="accordion-members">
+			<?php $item = 0; ?>
 			<?php foreach ($members as $k => $member): ?>
 				<h3 class="panel-title">
-			         <a href="#accordion<?php echo $bio['category_tid']; ?>" data-parent="#accordion<?php echo $bio['category_tid']; ?>" data-toggle="collapse" class="accordion-toggle <?php echo 'item_'.$key.'_'.$k; ?>" aria-expanded="true">
+			         <a href="#accordion<?php echo $bio['category_tid']; ?>" data-parent="#accordion<?php echo $bio['category_tid']; ?>" data-toggle="collapse" class="accordion-toggle <?php echo 'item_'.$key.'_'.$item ; ?>" aria-expanded="true">
 			           <?php echo $member->title; ?>
 			         </a>
 		      	</h3>
 		      	<div class="panel-body">
 		         <?php echo $member->body['und'][0]['value']; ?>
 		     	</div>
+		     <?php $item = $item+1; ?>
 			<?php endforeach; ?>
 		</div>
 		<?php endif; ?>
