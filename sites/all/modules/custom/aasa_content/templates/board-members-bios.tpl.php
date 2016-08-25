77f6cfecfd9101ecd2dc3c3f1de8eec00926913b
Carlos Leaño <carlos.anime@akendos.com>
2016-08-25 14:59:05 -0500

    ajustes

--- sites/all/modules/custom/aasa_content/templates/board-members-bios.tpl.php
+++ sites/all/modules/custom/aasa_content/templates/board-members-bios.tpl.php
@@ -10,20 +10,14 @@
 		<label for="edit-submitted-vice-president"><?php echo $bio['category_label']; ?></label>
 		<?php $members = $bio['category_bios']; ?>
 		<?php foreach ($members as $k => $member): ?>
-			<div class="panel panel-default">
-		       <div class="panel-heading">
-		          <h4 class="panel-title">
-		             <a href="#accordion<?php echo $bio['category_tid']; ?>" data-parent="#accordion<?php echo $bio['category_tid']; ?>" data-toggle="collapse" class="accordion-toggle" aria-expanded="true">
-		               <?php echo $member->title; ?>
-		             </a>
-		          </h4>
-		       </div>
-		       <div class="panel-collapse collapse in" id="accordion<?php echo $bio['category_tid']; ?>_<?php echo $member->nid; ?>" aria-expanded="true">
-		          <div class="panel-body">
-		             <?php echo $member->body['und'][0]['value']; ?>
-		          </div>
-		       </div>
-		    </div>
+			<h4 class="panel-title">
+             <a href="#accordion<?php echo $bio['category_tid']; ?>" data-parent="#accordion<?php echo $bio['category_tid']; ?>" data-toggle="collapse" class="accordion-toggle" aria-expanded="true">
+               <?php echo $member->title; ?>
+             </a>
+          </h4>
+          <div class="panel-body">
+             <?php echo $member->body['und'][0]['value']; ?>
+          </div>
 		<?php endforeach; ?>
 	</div>
 	<?php endforeach;?>
