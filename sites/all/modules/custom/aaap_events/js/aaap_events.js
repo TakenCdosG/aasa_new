efd979e75655f45be036cdbe5189a039cee13b5f
Adrián Andres Morelos Henriquez <ing.adrianmorelos@gmail.com>
2017-01-24 16:53:53 -0500

    Ajuste

--- sites/all/modules/custom/aaap_events/js/aaap_events.js
+++ sites/all/modules/custom/aaap_events/js/aaap_events.js
@@ -5,16 +5,22 @@
  */
 (function ($) {
     $(document).ready(function () { 
+
+    	function updateBoxDiscountCoupons(){
+    		var $aaap_discount_coupons = $('.webform-component--discount-codes');
+			var $payment_method = $( "#edit-submitted-payment-method option:selected" ).val();
+		 	if($payment_method != 'credit_card'){
+		 		$aaap_discount_coupons.addClass('hidden');
+		 	}else{
+		 		$aaap_discount_coupons.removeClass('hidden');
+		 	}
+    	}
     	var $aaap_discount_coupons = $('.webform-component--discount-codes');
     	if ( $aaap_discount_coupons.length > 0 ) {
 			$( "#edit-submitted-payment-method" ).change(function() {
-			 	var $payment_method = $( "#edit-submitted-payment-method option:selected" ).val();
-			 	if($payment_method != 'credit_card'){
-			 		$aaap_discount_coupons.addClass('hidden');
-			 	}else{
-			 		$aaap_discount_coupons.removeClass('hidden');
-			 	}
+			  updateBoxDiscountCoupons();
 			});
     	}
+    	updateBoxDiscountCoupons();
     });
 })(jQuery);
