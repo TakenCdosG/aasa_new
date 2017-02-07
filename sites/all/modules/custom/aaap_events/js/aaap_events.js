1e6996be7b021d7807f150011ae4e9cf0b96761b
Adrián Andres Morelos Henriquez <ing.adrianmorelos@gmail.com>
2017-02-07 17:09:09 -0500

    Ajustes Payment method

--- sites/all/modules/custom/aaap_events/js/aaap_events.js
+++ sites/all/modules/custom/aaap_events/js/aaap_events.js
@@ -24,17 +24,3 @@
     	updateBoxDiscountCoupons();
     });
 })(jQuery);
-
-jQuery(document).ready(function () { 
-	jQuery('.webform-component--payment-method').css( "display", "none" );
-	var select = ".webform-component--how-many-of-your-guests-will-be-attending-the-evening-event select";
-	jQuery( select ).change(function() {
-	  var number_guess = jQuery( select + " option:selected" ).val();
-	  number_guess = Number(number_guess);
-	  if(number_guess > 0){
-	  	jQuery('.webform-component--payment-method').css( "display", "block" );
-	  }else{
-	  	jQuery('.webform-component--payment-method').css( "display", "none" );
-	  }
-	});
-});
